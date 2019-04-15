0a480839c596f435688602169aa84c1df3ca7ca8
huahang <huahang.liu@gmail.com>
2019-04-15 15:53:21 +0800

    add a script to install deps for this project
    Signed-off-by: huahang <huahang.liu@gmail.com>

---
+++ thirdparty/install-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install libc-ares-dev
+sudo apt-get -y install libgflags-dev
+sudo apt-get -y install libgoogle-glog-dev
+sudo apt-get -y install libgoogle-perftools-dev
+sudo apt-get -y install libprotobuf-dev
+sudo apt-get -y install protobuf-compiler
+sudo apt-get -y install scons
+sudo apt-get -y update
+sudo apt-get -y upgrade
